2fbbfc7ec1feb608d239eeec18050a691dd1b47c
orionpgaete <pedro.gaete@pgcomputer.cl>
2021-03-18 20:36:13 -0300

    prueba de cambios

--- Clase_1/Clase_1/Program.cs
+++ Clase_1/Clase_1/Program.cs
@@ -4,9 +4,7 @@ namespace Clase_1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hola Mundo!");
-        }
+        static void Main(string[] args) => 
+          Console.WriteLine("Hola Mundo!");
     }
 }
