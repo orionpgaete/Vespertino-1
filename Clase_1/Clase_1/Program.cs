34056cbdfb6b80fd957ecfb92f144c3f6ac52c4e
orionpgaete <pedro.gaete@pgcomputer.cl>
2021-03-18 21:17:19 -0300

    uso del trim

--- Clase_1/Clase_1/Program.cs
+++ Clase_1/Clase_1/Program.cs
@@ -7,14 +7,17 @@ namespace Clase_1
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Cyan;
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Clear(); //100% el color de fondo
+
             Console.WriteLine("Hola Mundo!");
             Console.WriteLine("Ingrese nombre:");
-            string nombre = Console.ReadLine();
+            string nombre = Console.ReadLine().Trim();
+            //"    hola " .TrimStart(); hola "
+            //"  hola    " .TrimEnd();
             //Console.WriteLine("Su nombre es: " + nombre);
-            Console.WriteLine("Su nomnbre es: {0} {1}", nombre, 2021);
+            Console.WriteLine("Ingrese la edad: ");
+            string edadTx = Console.ReadLine();
+
+            Console.WriteLine("Su nomnbre es: {0} y la que tiene es: {1}", nombre, edadTx);
             Console.ReadKey();
         }
     }
