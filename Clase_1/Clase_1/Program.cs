f29997218bddce9de22537c269e25c5fb71fb5b6
orionpgaete <pedro.gaete@pgcomputer.cl>
2021-03-18 21:07:37 -0300

    imprimir con argumento

--- Clase_1/Clase_1/Program.cs
+++ Clase_1/Clase_1/Program.cs
@@ -8,6 +8,10 @@ namespace Clase_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hola Mundo!");
+            Console.WriteLine("Ingrese nombre:");
+            string nombre = Console.ReadLine();
+            //Console.WriteLine("Su nombre es: " + nombre);
+            Console.WriteLine("Su nomnbre es: {0} {1}", nombre, 2021);
             Console.ReadKey();
         }
     }
