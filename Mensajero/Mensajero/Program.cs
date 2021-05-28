f756f26623e55aab47bd33a6b8ac4b7cb24422f2
orionpgaete <pedro.gaete@pgcomputer.cl>
2021-05-27 21:20:18 -0400

    ultimos cambios

--- Mensajero/Mensajero/Program.cs
+++ Mensajero/Mensajero/Program.cs
@@ -1,9 +1,11 @@
 ﻿using MensajeroModel;
 using MensajeroModel.DAL;
+using ServidorSocketUtils;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,9 +17,13 @@ namespace Mensajero
         static bool Menu()
         {
             bool continuar = true;
+            string resp = "0";
             Console.WriteLine("¿Que quiere hacer?");
-            Console.WriteLine("1. Ingresar \n 2. Mostrar \n 0. Salir");
-            switch (Console.ReadLine().Trim())
+            Console.WriteLine(" 1. Ingresar \n 2. Mostrar \n 0. Salir");
+          
+            resp = Console.ReadLine().Trim();
+
+            switch (resp)
             {
                 case "1": Ingresar();
                     break;
@@ -34,6 +40,38 @@ namespace Mensajero
         static void IniciarServidor()
         {
             int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            ServerSocket servidor = new ServerSocket(puerto);
+            Console.WriteLine("Iniciando servidor en puerto {0}", puerto);
+            if (servidor.Iniciar())
+            {
+                while (true)
+                {
+                    Console.WriteLine("Esperando a Cliente...");
+                    Socket cliente = servidor.ObtenerCliente();
+                    Console.WriteLine("Cliente recibido");
+
+                    ClienteCom clienteCom = new ClienteCom(cliente);
+                    clienteCom.Escribir("Ingrese nombre: ");
+                    string nombre = clienteCom.Leer();
+                    clienteCom.Escribir("Ingrese texto: ");
+                    string texto = clienteCom.Leer();
+
+                    Mensaje mensaje = new Mensaje()
+                    {
+                        Nombre = nombre,
+                        Texto = texto,
+                        Tipo = "TCP"
+                    };
+                    mensajeDAL.AgregarMensaje(mensaje);
+                    clienteCom.Desconectar();
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error, no se puede iniciar Servidor en puerto {0}", puerto);
+            }
+
         }
         static void Main(string[] args)
         {
@@ -41,7 +79,7 @@ namespace Mensajero
             //2. el puerto tiene que ser configurable App.Config
             //3. cuando reciba un cliente, tiene que solicitar a ese cliente
             // el nombre y el texto, para agregar un nuevo mensaje con el tipo TCP
-
+           //IniciarServidor();
             while (Menu());
         }
 
