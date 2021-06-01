a603cc01c8e3fa99b1b07799e45ac2db838e578c
orionpgaete <pedro.gaete@pgcomputer.cl>
2021-05-31 22:37:41 -0400

    hola mundo asp.net

---
+++ HolaMundoASP/HolaMundoASP/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HolaMundoASP
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void SaludarBtn_Click(object sender, EventArgs e)
+        {
+            string nombre = this.nombretxt.Text.Trim();
+            this.mensajeH1.InnerText = "Hola "+nombre+ " ¡¡, para de flojear¡¡¡¡";
+        }
+    }
+}
